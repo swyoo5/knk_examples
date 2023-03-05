b791cd818d30382c92725d4a79e4fae0b81d8b53
Hong Kil-Dong <kdhong@gmail.com>
2023-03-05 22:13:26 +0900

    문제 추가

--- chapter2_1.c
+++ chapter2_1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-//Ex1
+/* 
+Ex1
+Write a program that uses printf to display the following picture on the screen:
+       *
+      *
+     *
+*   *
+ * *
+  *
+
+ */
+
 
 int main(void)
 {
